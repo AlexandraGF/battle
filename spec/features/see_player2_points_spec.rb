8a3d1675a8fbe3ba204d18b03f4663f51199429a
Alexy <alexandra.fina@yahoo.com>
2018-01-17 22:00:46 +0000

    Refactoring

--- spec/features/see_player2_points_spec.rb
+++ spec/features/see_player2_points_spec.rb
@@ -1,12 +1,12 @@
 require './app.rb'
 
-feature 'Testing to have names' do
-  scenario 'checks that we have submited 2 names' do
+feature 'See the points' do
+  scenario 'checks player2\'s points' do
     # visit('/')
     # fill_in :player_1, with: 'Alexa'
     # fill_in :player_2, with: 'David'
     # click_button 'Submit'
     sign_in_and_play
-    expect(page).to have_content 'Alexa vs. David'
+    expect(page).to have_content 'David: 20 Hit Points'
   end
 end
